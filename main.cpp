8551e56d9635c1c063385f2055e77407c7ebb0b5
abderrahman <karamabderrahmane30@gmail.com>
2025-05-12 16:14:40 +0100

    update

--- main.cpp
+++ main.cpp
@@ -9,16 +9,12 @@ using namespace std;
 bool isPortOpen(const string& ip, int port) {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) return false;
-
     sockaddr_in target;
     target.sin_family = AF_INET;
     target.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &target.sin_addr);
-
-    // Tentative de connexion
     int result = connect(sockfd, (sockaddr*)&target, sizeof(target));
     close(sockfd);
-
     return result == 0;
 }
 
@@ -32,14 +28,12 @@ int main() {
     cin >> startPort;
     cout << "Port fin: ";
     cin >> endPort;
-
-    for (int port = startPort; port <= endPort; ++port) {
-        if (isPortOpen(ip, port)) {
+    for (int port = startPort; port <= endPort; ++port)
+    {
+        if (isPortOpen(ip, port))
             cout << "Port " << port << " est OUVERT ✅" << endl;
-        } else {
+        else
             cout << "Port " << port << " est fermé ❌" << endl;
-        }
     }
-
     return 0;
 }
